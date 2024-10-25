aaa20b5ba80f12f7f93a4e3b1988cba04526dd24
LeeJeongWook <leejw0709@gmail.com>
2024-10-26 00:00:01 +0900

    Update [PCCP 기출문제] 1번 붕대 감기.c

--- Lv.1/[PCCP 기출문제] 1번 붕대 감기.c
+++ Lv.1/[PCCP 기출문제] 1번 붕대 감기.c
@@ -9,17 +9,41 @@ int solution(int bandage[], size_t bandage_len, int health, int** attacks, size_
     int max_health = health;
     int attack_idx = 0;
     int length = attacks[attacks_rows - 1][0];
+    int time = 0;
+    bool flag = false;
     
     for(int i = 1; i <= length; i++){
-        /* 몬스터가 공격할 때 */
+        //몬스터에게 공격받을 떄
         if(i == attacks[attack_idx][0]){
+            health -= attacks[attack_idx][1];
+            if(attack_idx < attacks_rows-1)
+                attack_idx++;   //다음 몬스터 인덱스
+            if(flag){
+                time = 0;  //연속성공 초기화
+                flag = false;
+            }
         }
         
-        
-        if(health < max_health){
+        if(flag){
+            time++;
+            health += bandage[1];
             
+            if(time == bandage[0]){
+                health += bandage[2];
+                time = 0;
+            }
         }
+        
+        if(health <= 0){
+            answer = -1;
+            return answer;
+        }
+        else if(0 < health && health < max_health)
+            flag = true;
+            
+        else if(max_health <= health) //체력이 최대치일때
+            flag = false;
     }
-    
+    answer = health;
     return answer;
 }
