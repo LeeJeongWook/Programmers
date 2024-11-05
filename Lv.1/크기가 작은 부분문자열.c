8080f687c247ddb22c31115fb4ffb99d2883870e
leejw0709@gmail.com <leejw0709@gmail.com>
2024-11-05 11:03:42 +0900

    -

--- Lv.1/크기가 작은 부분문자열.c
+++ Lv.1/크기가 작은 부분문자열.c
@@ -4,30 +4,38 @@
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(const char* t, const char* p) {
+    /* Input: 문자열 t, 문자열 p */
     int answer = 0;
     int p_idx;
     int t_len = strlen(t);
     int p_len = strlen(p);
     bool flag = false;
     
+    /* 1. 문자열 t를 문자열 p 길이만큼 순차적으로 자르기 */
+    /* 2. 자른 문자열의 자릿수마다 비교 */
     for(int i = 0; i <= t_len - p_len; i++){
         p_idx = 0;
         flag = false;
         for(int j = i; j < i + p_len; j++){
+            /* 2.1. 같을 경우 계속 탐색 */
             if(t[j] == p[p_idx]) flag = true;
+            /* 2.2. 작을 경우 answer + 1 */
             else if(t[j] < p[p_idx]){
                 flag = false;
                 answer++;
                 break;
             }
+            /* 2.3. 클 경우 다음 문자열 탐색 */
             else if(t[j] > p[p_idx]){
                 flag = false;
                 break;
             }
             p_idx++;
         }
+        /* 2.4. 자른 문자열이 모드 같을 경우 answer + 1 */
         if(flag) answer++;
     }
     
+    /* Output: 부분 문자열이 나타내는 수가 p가 나타내는 수보다 작거나 같은 것이 나오는 횟수 */
     return answer;
 }
--- Lv.2/최솟값 만들기.c
+++ Lv.2/최솟값 만들기.c
@@ -2,10 +2,12 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+/* Func: 오름차순 정렬 */
 int ascending(const void* a, const void* b){
     return *(int*)a - *(int*)b;
 }
 
+/* Func: 내림차순 정렬 */
 int descending(const void* a, const void* b){
     return *(int*)b - *(int*)a;
 }
@@ -13,13 +15,18 @@ int descending(const void* a, const void* b){
 // A_len은 배열 A의 길이입니다.
 // B_len은 배열 B의 길이입니다.
 int solution(int A[], size_t A_len, int B[], size_t B_len) {
+    /* Input: 배열 A(A[], A_len), 배열 B(B[], B_len) */
     int answer = 0;
     
+    /* 1. 배열 A를 오름차순으로 정렬 */
     qsort(A, A_len, sizeof(int), ascending);
+    /* 2. 배열 B를 내림차순으로 정렬 */
     qsort(B, B_len, sizeof(int), descending);
     
+    /* 3. 배열 A, B를 인덱스 순서대로 곱한 값을 누적 */
     for(int i = 0; i < A_len; i++)
         answer += A[i] * B[i];
     
+    /* Output: 누적된 최솟값 */
     return answer;
 }
